96dc31ddfe3f6ed54acbd3bf66accef6be1a0a75
blackmath45 <46330582+blackmath45@users.noreply.github.com>
2022-12-26 18:31:23 +0100

    Update library

--- SHT1X_modbus_RS485/SHT1X_modbus_RS485.ino
+++ SHT1X_modbus_RS485/SHT1X_modbus_RS485.ino
@@ -3,62 +3,10 @@
 
   Type de carte : Arduino nano (old bootloader)
   
-  Modification de la librairie "SHT1x" nécessaire (à tester) :
-  - Dans le fichier "SHT1x.cpp" fonction "void SHT1x::waitForResultSHT(int _dataPin)" :
-	Passer le delay(10) en delay(1)
-	Voir également pour basser tous les délais
+  Modification de la librairie "SHT1x" nécessaire => prendre la version jointe.
 
-  La modification ci-dessous n'est plus vraie avec la lib 1.0.7 d'ArduinoModbus
-  
-  Modification de la librairie "ArduinoModbus" nécessaire :
-  - Dans le fichier "ModbusRTUServer.cpp" fonction "void ModbusRTUServerClass::poll()" :
-      int ModbusRTUServerClass::poll()
-      {
-        uint8_t request[MODBUS_RTU_MAX_ADU_LENGTH];
-        int rc;
-      
-        rc = -1;
-        
-        int requestLength = modbus_receive(_mb, request);
-      
-        if (requestLength > 0)
-        {
-          rc = modbus_reply(_mb, request, requestLength, &_mbMapping);
-        }
-      
-        return rc;
-      }
+  ArduinoModbus V1.0.7
 
-  - Dans le fichier "ModbusRTUServer.h" :
-      remplacer   virtual void poll(); par   virtual int poll();
-
-  - Dans le fichier "ModbusServer.h" :
-      remplacer   virtual void poll() = 0; par   virtual int poll() = 0;;
-
-  - Dans le fichier "ModbusTCPServer.h" :
-      remplacer   virtual void poll(); par   virtual int poll();
-
-  - Dans le fichier "ModbusTCPServer.cpp" fonction "void ModbusRTUServerClass::poll()" :
-      int ModbusTCPServer::poll()
-      {
-        int rc;
-      
-        rc = -1;  
-        
-        if (_client != NULL) {
-          uint8_t request[MODBUS_TCP_MAX_ADU_LENGTH];
-      
-          int requestLength = modbus_receive(_mb, request);
-      
-          if (requestLength > 0)
-        {
-            modbus_reply(_mb, request, requestLength, &_mbMapping);
-          }
-        }
-      
-        return rc;  
-      }
-            
 */
 #include <ArduinoModbus.h>
 #include <SHT1x.h>
@@ -70,6 +18,8 @@
 
 #define dataPin 4
 #define clockPin 3
+
+//RE/DE RS485 sur D2
 
 //#define SERIAL_DEBUG
 //---------------------------------------------------------------
@@ -121,8 +71,9 @@ void ReadTemperature()
   int mb_res;
   
   // Read values from the sensor
-  temp_c = sht1x.readTemperatureC();
-  humidity = sht1x.readHumidity();
+  sht1x.readAll();
+  temp_c = sht1x.getTemperatureC();
+  humidity = sht1x.getHumidity();  
 
 #if defined(SERIAL_DEBUG)
   Serial.print("Temperature raw : ");
